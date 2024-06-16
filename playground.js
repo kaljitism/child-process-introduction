37a164fb3bcbcece0e85377dce425372040a5d52
installer <installer@gmail.com>
2024-06-17 03:21:21 +0530

    Played around with shells and processes.

--- playground.js
+++ playground.js
@@ -1,6 +1,6 @@
 const {spawn, exec} = require('node:child_process');
 
-const subprocess = spawn('echo', ["something string", '|', 'tr', " ", "\n"]);
+const subprocess = spawn('zsh', ['./scripts.sh']);
 
 subprocess.stdout.on('data', chunk => {
   console.log(chunk.toString());
@@ -10,8 +10,12 @@ subprocess.on('error', error => {
   console.log(`Error: ${error}`);
 });
 
-exec('echo "something string" | tr " " "\n"', (error, stdout, stderr) => {
-  if (error) console.log(error);
+// console.log(process.env.PATH);
+
+exec('echo "something string" | tr " " "\n"', {
+  shell:'/usr/share/bash'
+}, (error, stdout, stderr) => {
   if (stderr) console.log(stderr);
   if (stdout) console.log(stdout);
+  console.log('Welcome in ther shelll');
 });
