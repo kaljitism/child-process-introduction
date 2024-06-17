dc4870e99ee9856c4034ac19b44f7bd00fd0fab0
installer <installer@gmail.com>
2024-06-18 04:33:05 +0530

    stdin, stdout, stderr

--- playground.c
+++ playground.c
@@ -3,14 +3,26 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+//
+//   for (int i = 0; i < argc; i++) {
+//     printf("argv number %d is: %s\n", i, argv[i]);
+//   }
 
-  for (int i = 0; i < argc; i++) {
-    printf("argv number %d is: %s\n", i, argv[i]);
+//   printf("Process ID: %d\n", getpid());
+//   printf("Parent Process ID: %d\n", getppid());
+//   printf("Some environment variable: %s\n", getenv('MODE'));
+
+  fprintf(stdout, "Some text for stdout. Coming from C.");
+  fprintf(stderr, "Some text from stderr. Coming from C.");
+
+  char c = fgetc(stdin);
+  while (c != EOF) {
+    fprintf(stdout, "%c", c);
+
+    fflush(stdout);
+
+    c = fgetc(stdin);
   }
-
-  printf("Process ID: %d\n", getpid());
-  printf("Parent Process ID: %d\n", getppid());
-  printf("Some environment variable: %s\n", getenv('MODE'));
 
   return 0;
 }
