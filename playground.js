580e62908e2f5b0eab41320500cdc57dc9176706
installer <installer@gmail.com>
2024-06-18 04:33:47 +0530

    final optimizations

--- playground.js
+++ playground.js
@@ -1,42 +1,42 @@
 const {spawn, exec} = require('node:child_process');
-
-// To set an environment variable, we do -
-// >> export FOO='bar'
-// To use them we refer them as process.env.FOO
-// To delete them, we execute - >> unset FOO
-
-// ---------- Environment Variables ---------- //
-
-console.log(`PATH: ${process.env.PATH}`);
-console.log(`ARGUMENTS: ${process.argv}`);
-console.log(`Process ID: ${process.pid}`);
-console.log(`Parent Process ID: ${process.ppid}`);
-console.log(`Environment Variables: ${process.env}`);
-console.log(`Present Working Directory : ${process.env.PWD}`);
-
-// ---------- Child Processes ---------- //
-
-const subprocess = spawn(
-    'zsh',
-    ['./scripts.sh'],
-    );
-
-subprocess.stdout.on('data', chunk => {
-  console.log(chunk.toString());
-});
-
-subprocess.on('error', error => {
-  console.log(`Error: ${error}`);
-});
-
-exec('echo "something string" | tr " " "\n"', {
-  shell:'/usr/share/bash'
-}, (error, stdout, stderr) => {
-  if (stderr) console.log(stderr);
-  if (stdout) console.log(stdout);
-  console.log('Welcome in ther shelll');
-});
-
+//
+// // To set an environment variable, we do -
+// // >> export FOO='bar'
+// // To use them we refer them as process.env.FOO
+// // To delete them, we execute - >> unset FOO
+//
+// // ---------- Environment Variables ---------- //
+//
+// console.log(`PATH: ${process.env.PATH}`);
+// console.log(`ARGUMENTS: ${process.argv}`);
+// console.log(`Process ID: ${process.pid}`);
+// console.log(`Parent Process ID: ${process.ppid}`);
+// console.log(`Environment Variables: ${process.env}`);
+// console.log(`Present Working Directory : ${process.env.PWD}`);
+//
+// // ---------- Child Processes ---------- //
+//
+// const subprocess = spawn(
+//     'zsh',
+//     ['./scripts.sh'],
+//     );
+//
+// subprocess.stdout.on('data', chunk => {
+//   console.log(chunk.toString());
+// });
+//
+// subprocess.on('error', error => {
+//   console.log(`Error: ${error}`);
+// });
+//
+// exec('echo "something string" | tr " " "\n"', {
+//   shell:'/usr/share/bash'
+// }, (error, stdout, stderr) => {
+//   if (stderr) console.log(stderr);
+//   if (stdout) console.log(stdout);
+//   console.log('Welcome in ther shelll');
+// });
+//
 
 // ---------- C Child Process ---------- //
 
@@ -58,6 +58,9 @@ cProcess.stdout.on('data', chunk => {
   console.log(chunk.toString());
 });
 
-cProcess.on('error', error => {
-  console.log(`Error: ${error}`);
+cProcess.stderr.on('data', chunk => {
+  console.log('Got this stderr from the C app: ', chunk.toString());
 });
+
+cProcess.stdin.write('Some text that is coming from Node....');
+cProcess.stdin.end();
