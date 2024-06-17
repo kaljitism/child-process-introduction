0acd97e69f8e5282feda62fc5215bc7147fd299b
installer <installer@gmail.com>
2024-06-18 04:13:22 +0530

    shell script

--- scripts.sh
+++ scripts.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+
 myfunc() {
-ls
-x=120
-y=500
-sleep 5 
-echo $(($x+$y))
-echo $1 | tr ' ' '\n'
+  ls
+  x=120
+  y=500
+  sleep 5
+  echo $(($x+$y))
+  echo $1 | tr ' ' '\n'
 }
 
 myfunc 'some jibberish gibberish pr siaiisj iqagsi asuhs auu yYVS'
